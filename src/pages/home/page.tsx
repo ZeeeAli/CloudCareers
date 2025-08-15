a3f04949586101267c585deb912c4217b1b25c80
Zeeshan Ali <za4116793@gmail.com>
2025-08-15 19:02:54 +0500

    edited

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -271,7 +271,7 @@ export default function Home() {
             </div>
           </div>
           <div className="border-t border-gray-800 pt-8 mt-8 text-center text-gray-400">
-            <p>&copy; 2024 RemoteJobs. All rights reserved.</p>
+            <p>&copy; 2024 CloudCareers. All rights reserved.</p>
           </div>
         </div>
       </footer>
